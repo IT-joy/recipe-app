0a51cb7f4d8b936f7b608682e9f6d971092818c8
Julia <ulamind@gmail.com>
2025-04-28 01:14:10 +0400

    Add content for the Section on the Home Page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,8 +1,5 @@
 <script setup lang="ts">
-const { data, error } = await useAsyncData('recipes', () =>
-  $fetch('https://dummyjson.com/recipes?limit=12')
-);
-
+const { data, error } = await useFetch("https://dummyjson.com/recipes?limit=12");
 </script>
 
 <template>
@@ -28,6 +25,37 @@ const { data, error } = await useAsyncData('recipes', () =>
             densities="x1"
             alt=""
           />
+        </div>
+      </div>
+    </section>
+    <section class="py-20 container">
+      <h2 class="text-3xl lg:text-5xl mb-2">Discover, Create, Share</h2>
+      <p class="text-lg lg:text-xl mb-8">Check out our most popular recipes!</p>
+      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-4 gap-y-8">
+        <div class="flex flex-col shadow rounded-md">
+          <img src="" alt="" class="rounded-t-md" />
+          <div class="flex flex-col py-6 px-4 flex-1">
+            <p class="text-xl lg:text-2xl font-semibold mb-2">Recipe Name</p>
+            <div class="font-normal w-full bg-white/80 flex gap-8 text-lg lg:text-xl mb-4 mt-auto">
+              <div class="flex items-center gap-1">
+                <Icon name="mdi:clock-time-eight-outline" style="color: #f79f1a" />
+                <span>Cook Time</span>
+              </div>
+              <div class="flex items-center gap-1">
+                <Icon name="mdi:fire" style="color: #f79f1a" />
+                <span>Calories</span>
+              </div>
+              <div class="flex items-center gap-1">
+                <Icon name="mdi:star" style="color: #f79f1a" />
+                <span>Rating (Rating Count)</span>
+              </div>
+            </div>
+            <button
+              class="px-4 py-2 text-white self-start bg-dodgeroll-gold rounded-md text-base lg:text-lg cursor-pointer"
+            >
+              View
+            </button>
+          </div>
         </div>
       </div>
     </section>
